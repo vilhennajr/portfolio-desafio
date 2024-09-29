24b33e959bb868ed847c30cdb2db6c613ab2bbe0
Flaviano Vilhena <flavianovilhenna@gmail.com>
2024-09-29 14:04:55 -0300

    feat(task-2): header and logo component

---
+++ web/frontend/src/components/atoms/Header/index.tsx
@@ -0,0 +1,9 @@
+import Logo from "../Logo";
+
+const Header = () => (
+  <div className="w-full h-[74px] bg-[#C4170C] flex justify-center items-center">
+    <Logo />
+  </div>
+);
+
+export default Header;
--- web/frontend/src/components/templates/InitialPage/index.tsx
+++ web/frontend/src/components/templates/InitialPage/index.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import Header from "@/components/atoms/Header";
+import { Box } from "@chakra-ui/react";
+
 const InitialPage = () => {
-  return <></>;
+  return (
+    <Box className="justify-center flex flex-col items-center">
+      <Header />
+    </Box>
+  );
 };
 
 export default InitialPage;
